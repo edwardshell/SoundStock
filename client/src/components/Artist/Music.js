34d57fa7193196d642cd038fcb78cc13d7e3e6c1
Ed Shell <shelleg@g.cofc.edu>
2018-08-15 14:26:41 -0400

    files

--- client/src/components/Artist/Music.js
+++ client/src/components/Artist/Music.js
@@ -1,27 +1,29 @@
 import React from "react";
 
-const styles = {
-    song: {
-        position: "relative",
-        width: "260px",
-        height: "25px",
-        overflow: "hidden",
-        marginLeft: "20px"
-    },
-    controls: {
-        position: "absolute",
-        top: "-267px",
-        left: "-5px"
-    }
-}
+// const styles = {
+//     song: {
+//         position: "relative",
+//         width: "260px",
+//         height: "25px",
+//         overflow: "hidden",
+//         marginLeft: "20px"
+//     },
+//     controls: {
+//         position: "absolute",
+//         top: "-267px",
+//         left: "-5px"
+//     }
+// }
 const Music = props => (
-  <div style={styles.song}>
-    <div style={styles.controls}>
-      <p>Play</p>
+  <div>
+    <div>
       <iframe
         title="Song Link"
         width="300"
-        height="300"
+        height="24"
+        frameborder="0"
+        autohide="0"
+        allowFullScreen
         src={props.src}
       />
     </div>
